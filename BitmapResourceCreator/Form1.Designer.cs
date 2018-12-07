b3a013d6a7fb8bf3fbf378d70b1570b84c2a715f
Ben Melikant <bmelikant@clango.com>
2018-12-07 08:08:14 -0500

    Add project files.

---
+++ BitmapResourceCreator/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace BitmapResourceCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newFontSetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFontSetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToCCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gb_characterSelectBox = new System.Windows.Forms.GroupBox();
+            this.gb_fontMapPreviewBox = new System.Windows.Forms.GroupBox();
+            this.gb_characterPreviewBox = new System.Windows.Forms.GroupBox();
+            this.btn_addCharacter = new System.Windows.Forms.Button();
+            this.btn_clearCharacter = new System.Windows.Forms.Button();
+            this.nud_indexUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pb_previewBox = new System.Windows.Forms.PictureBox();
+            this.btn_invertCharacter = new System.Windows.Forms.Button();
+            this.pb_fontMapPreview = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.gb_fontMapPreviewBox.SuspendLayout();
+            this.gb_characterPreviewBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_indexUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_previewBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_fontMapPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(585, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newFontSetToolStripMenuItem,
+            this.saveFontSetToolStripMenuItem,
+            this.exportToCCToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // newFontSetToolStripMenuItem
+            // 
+            this.newFontSetToolStripMenuItem.Name = "newFontSetToolStripMenuItem";
+            this.newFontSetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newFontSetToolStripMenuItem.Text = "New Font Set";
+            // 
+            // saveFontSetToolStripMenuItem
+            // 
+            this.saveFontSetToolStripMenuItem.Name = "saveFontSetToolStripMenuItem";
+            this.saveFontSetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveFontSetToolStripMenuItem.Text = "Save Font Set";
+            // 
+            // exportToCCToolStripMenuItem
+            // 
+            this.exportToCCToolStripMenuItem.Name = "exportToCCToolStripMenuItem";
+            this.exportToCCToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToCCToolStripMenuItem.Text = "Export to C/C++";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // gb_characterSelectBox
+            // 
+            this.gb_characterSelectBox.AutoSize = true;
+            this.gb_characterSelectBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gb_characterSelectBox.Location = new System.Drawing.Point(12, 27);
+            this.gb_characterSelectBox.Name = "gb_characterSelectBox";
+            this.gb_characterSelectBox.Size = new System.Drawing.Size(6, 19);
+            this.gb_characterSelectBox.TabIndex = 1;
+            this.gb_characterSelectBox.TabStop = false;
+            this.gb_characterSelectBox.Text = "Define Character";
+            // 
+            // gb_fontMapPreviewBox
+            // 
+            this.gb_fontMapPreviewBox.AutoSize = true;
+            this.gb_fontMapPreviewBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gb_fontMapPreviewBox.Controls.Add(this.pb_fontMapPreview);
+            this.gb_fontMapPreviewBox.Location = new System.Drawing.Point(12, 260);
+            this.gb_fontMapPreviewBox.Name = "gb_fontMapPreviewBox";
+            this.gb_fontMapPreviewBox.Size = new System.Drawing.Size(97, 94);
+            this.gb_fontMapPreviewBox.TabIndex = 2;
+            this.gb_fontMapPreviewBox.TabStop = false;
+            this.gb_fontMapPreviewBox.Text = "Font Map Preview";
+            // 
+            // gb_characterPreviewBox
+            // 
+            this.gb_characterPreviewBox.AutoSize = true;
+            this.gb_characterPreviewBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gb_characterPreviewBox.Controls.Add(this.pb_previewBox);
+            this.gb_characterPreviewBox.Location = new System.Drawing.Point(179, 27);
+            this.gb_characterPreviewBox.Name = "gb_characterPreviewBox";
+            this.gb_characterPreviewBox.Size = new System.Drawing.Size(142, 216);
+            this.gb_characterPreviewBox.TabIndex = 3;
+            this.gb_characterPreviewBox.TabStop = false;
+            this.gb_characterPreviewBox.Text = "Character Preview";
+            // 
+            // btn_addCharacter
+            // 
+            this.btn_addCharacter.Location = new System.Drawing.Point(352, 86);
+            this.btn_addCharacter.Name = "btn_addCharacter";
+            this.btn_addCharacter.Size = new System.Drawing.Size(109, 35);
+            this.btn_addCharacter.TabIndex = 4;
+            this.btn_addCharacter.Text = "Add Character";
+            this.btn_addCharacter.UseVisualStyleBackColor = true;
+            this.btn_addCharacter.Click += new System.EventHandler(this.btn_addCharacterClicked);
+            // 
+            // btn_clearCharacter
+            // 
+            this.btn_clearCharacter.Location = new System.Drawing.Point(352, 127);
+            this.btn_clearCharacter.Name = "btn_clearCharacter";
+            this.btn_clearCharacter.Size = new System.Drawing.Size(109, 34);
+            this.btn_clearCharacter.TabIndex = 5;
+            this.btn_clearCharacter.Text = "Clear Character";
+            this.btn_clearCharacter.UseVisualStyleBackColor = true;
+            this.btn_clearCharacter.Click += new System.EventHandler(this.btn_clearSelectBoxes);
+            // 
+            // nud_indexUpDown
+            // 
+            this.nud_indexUpDown.Location = new System.Drawing.Point(437, 39);
+            this.nud_indexUpDown.Name = "nud_indexUpDown";
+            this.nud_indexUpDown.Size = new System.Drawing.Size(54, 20);
+            this.nud_indexUpDown.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Character Index";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pb_previewBox
+            // 
+            this.pb_previewBox.Location = new System.Drawing.Point(6, 19);
+            this.pb_previewBox.Name = "pb_previewBox";
+            this.pb_previewBox.Size = new System.Drawing.Size(130, 178);
+            this.pb_previewBox.TabIndex = 0;
+            this.pb_previewBox.TabStop = false;
+            // 
+            // btn_invertCharacter
+            // 
+            this.btn_invertCharacter.Location = new System.Drawing.Point(352, 167);
+            this.btn_invertCharacter.Name = "btn_invertCharacter";
+            this.btn_invertCharacter.Size = new System.Drawing.Size(109, 34);
+            this.btn_invertCharacter.TabIndex = 8;
+            this.btn_invertCharacter.Text = "Invert Character";
+            this.btn_invertCharacter.UseVisualStyleBackColor = true;
+            this.btn_invertCharacter.Click += new System.EventHandler(this.btn_invertSelection);
+            // 
+            // pb_fontMapPreview
+            // 
+            this.pb_fontMapPreview.Location = new System.Drawing.Point(6, 19);
+            this.pb_fontMapPreview.Name = "pb_fontMapPreview";
+            this.pb_fontMapPreview.Size = new System.Drawing.Size(85, 56);
+            this.pb_fontMapPreview.TabIndex = 0;
+            this.pb_fontMapPreview.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(585, 454);
+            this.Controls.Add(this.btn_invertCharacter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nud_indexUpDown);
+            this.Controls.Add(this.btn_clearCharacter);
+            this.Controls.Add(this.btn_addCharacter);
+            this.Controls.Add(this.gb_characterPreviewBox);
+            this.Controls.Add(this.gb_fontMapPreviewBox);
+            this.Controls.Add(this.gb_characterSelectBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.gb_fontMapPreviewBox.ResumeLayout(false);
+            this.gb_characterPreviewBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_indexUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_previewBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_fontMapPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newFontSetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFontSetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToCCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.GroupBox gb_characterSelectBox;
+        private System.Windows.Forms.GroupBox gb_fontMapPreviewBox;
+        private System.Windows.Forms.GroupBox gb_characterPreviewBox;
+        private System.Windows.Forms.Button btn_addCharacter;
+        private System.Windows.Forms.Button btn_clearCharacter;
+        private System.Windows.Forms.NumericUpDown nud_indexUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pb_previewBox;
+        private System.Windows.Forms.Button btn_invertCharacter;
+        private System.Windows.Forms.PictureBox pb_fontMapPreview;
+    }
+}
+
---
+++ BitmapResourceCreator/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapResourceCreator
+{
+    public partial class Form1 : Form
+    {
+        /** private member variables */
+        private int fontPixelsX = 10;           // font is 8 pixels wide with 2 pixels of padding space
+        private int fontPixelsY = 14;           // font is 12 pixels high with 2 pixels padding space
+        private int characterSetSize = 128;     // there are 128 characters in the initial set (assumes ASCII)
+
+        private List<CheckBox> cb_designerCheckBoxes;   // the checkbox controls for designing the font
+        private List<int[]> characterValues;            // the bitmap data stored as a list of int ptrs
+
+        public Form1()
+        {
+            InitializeComponent();
+            createCheckBoxControls();
+            resizePreviewBox();
+            resizeFontMapPreviewBox();
+            createCharacterValueArray();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /**
+         * @fn createCheckBoxControls()
+         * This function will create a series of button-style checkboxes in the design group box
+         * 
+         */
+        private void createCheckBoxControls() {
+            Point currentLocation = new Point(10,20);
+            cb_designerCheckBoxes = new List<CheckBox>();
+
+            // create combo box rows
+            for (int i = 0; i < fontPixelsY; i++) {
+                for (int j = 0; j < fontPixelsX; j++) {
+                    CheckBox newBox = new CheckBox {
+                        Size = new Size(10, 10),
+                        Location = new Point(currentLocation.X, currentLocation.Y),
+                        Appearance = Appearance.Button
+                    };
+
+                    newBox.CheckedChanged += gb_characterSelectBoxChanged;
+                    gb_characterSelectBox.Controls.Add(newBox);
+                    cb_designerCheckBoxes.Add(newBox);
+
+                    currentLocation.X += 11;
+                }
+
+                currentLocation.X = 10;
+                currentLocation.Y += 11;
+            }
+
+            // resize the group box
+        }
+
+        private void resizeSelectGroupBox() {
+            // compute the number of pixels wide and high
+
+        }
+
+        private void resizeFontMapPreviewBox() {
+            int widthMultiplier = 30;   // 30 chars wide
+            int heightMultiplier = (characterSetSize / widthMultiplier) + 1;
+
+            pb_fontMapPreview.Size = new Size {
+                Width = (fontPixelsX * 2) * widthMultiplier,
+                Height = (fontPixelsY * 2) * heightMultiplier
+            };
+
+            Bitmap b = new Bitmap(pb_fontMapPreview.Width, pb_fontMapPreview.Height);
+            Graphics g = Graphics.FromImage(b);
+            g.Clear(Color.Black);
+
+            pb_fontMapPreview.Image = b;
+        }
+
+        private void resizePreviewBox() {
+            // compute the width and height of the preview and clear the bitmap
+            pb_previewBox.Size = new Size {
+                Width = fontPixelsX * 10,
+                Height = fontPixelsY * 10
+            };
+
+            Bitmap testBmp = new Bitmap(pb_previewBox.Size.Width, pb_previewBox.Size.Height);
+            Graphics g = Graphics.FromImage(testBmp);
+            g.Clear(Color.Black);
+
+            pb_previewBox.Image = testBmp;
+        }
+
+        private void drawPreviewBox() {
+            Bitmap b = (Bitmap) pb_previewBox.Image;
+            Graphics g = Graphics.FromImage(b);
+
+            // read each line of the select boxes, set preview accordingly. Each preview 'pixel' is 10 pixels square
+            for (int i = 0; i < fontPixelsY; i++) {
+                for (int j = 0; j < fontPixelsX; j++) {
+                    CheckBox currentElement = cb_designerCheckBoxes.ElementAt(i*fontPixelsX+j);
+                    // draw a white square here!
+                    Rectangle r = new Rectangle {
+                        X = j*10,
+                        Y = i*10,
+                        Width = 10,
+                        Height = 10
+                    };
+
+                    Pen p = Pens.Black;
+                    if (currentElement.Checked) {
+                        p = Pens.White;
+                    }
+
+                    g.FillRectangle(p.Brush, r);
+                }
+            }
+
+            pb_previewBox.Image = b;
+        }
+
+        /**
+         * @fn gb_characterSelectBoxChanged(object sender, EventArgs e)
+         * This function will update the background color of the check box when selected
+         */
+        private void gb_characterSelectBoxChanged(object sender, EventArgs e) {
+            CheckBox senderObject = (CheckBox) sender;
+            if (senderObject.Checked) {
+                senderObject.BackColor = Color.Black;
+            } else {
+                senderObject.BackColor = System.Drawing.SystemColors.Control;
+            }
+
+            /** draw the current preview */
+            drawPreviewBox();
+        }
+
+        private void btn_clearSelectBoxes(object sender, EventArgs e) {
+            foreach (CheckBox c in cb_designerCheckBoxes) {
+                c.Checked = false;
+            }
+            drawPreviewBox();
+        }
+
+        private void btn_invertSelection(object sender, EventArgs e) {
+            foreach (CheckBox c in cb_designerCheckBoxes) {
+                if (c.Checked) {
+                    c.Checked = false;
+                } else {
+                    c.Checked = true;
+                }
+            }
+            drawPreviewBox();
+        }
+
+        private void btn_addCharacterClicked(object sender, EventArgs e) {
+            int index = (int) nud_indexUpDown.Value;
+            int [] charData = new int[fontPixelsY];
+            // copy in each pixel from each row of the character
+            for (int i = 0; i < fontPixelsY; i++) {
+                int rowData = 0;
+                int top = fontPixelsX - 1;
+
+                for (int j = 0; j < fontPixelsX; j++) {
+                    int cbIndex = (i*fontPixelsX)+top;
+                    if (cb_designerCheckBoxes.ElementAt(cbIndex).Checked) {
+                        rowData |= (1 << j);
+                    }
+                    top--;
+                }
+                charData[i] = rowData;
+            }
+            characterValues[index] = charData;
+            drawFontMapPreviewBox();
+        }
+
+        private void createCharacterValueArray() {
+            characterValues = new List<int[]>(characterSetSize);
+            // initialize each array
+            for (int i = 0; i < characterSetSize; i++) {
+                characterValues.Insert(i, new int[fontPixelsY]);
+            }
+        }
+
+        private void drawFontMapPreviewBox() {
+            // draw each character in the current map
+            Bitmap b = (Bitmap) pb_fontMapPreview.Image;
+            Graphics g = Graphics.FromImage(b);
+            g.Clear(Color.Black);
+
+            Rectangle r = new Rectangle(0,0,4,4);
+
+            /** draw each character */
+            foreach (int[] charData in characterValues) {
+                /** draw each row of character data */
+                for (int i = 0; i < fontPixelsY; i++) {
+                    int rowDefinition = charData[i];
+                    /** draw each x pixel */
+                    for (int j = fontPixelsX-1; j >= 0; j--) {
+                        if ((rowDefinition & (1 << j)) > 0) {
+                            g.FillRectangle(Pens.White.Brush, r);
+                        }
+                        r.X += 2;
+                    }
+                    r.X = 0;
+                    r.Y += 2;
+                }
+            }
+
+            pb_fontMapPreview.Image = b;
+        }
+    }
+}
